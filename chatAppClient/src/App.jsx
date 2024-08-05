72b4ab6c55512c1beed4e086a5750769d6b3f5a4
GAURAV <gaurav1993rajput@gmail.com>
2024-08-05 20:05:00 +0530

    initial set-up2

--- chatAppClient/src/App.jsx
+++ chatAppClient/src/App.jsx
@@ -2,6 +2,9 @@ import React, {lazy} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 const Home = lazy(()=>import("./pages/Home"))
 const Login = lazy(()=>import("./pages/Login"))
+const Chat = lazy(()=>import("./pages/Chat"))
+const Groups = lazy(()=>import("./pages/Groups"))
+
 
 
 const App = () => {
@@ -10,6 +13,8 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
+        <Route path='/chat/:chatId' element={<Chat/>} />
+        <Route path='/groups' element={<Groups/>} />
       </Routes>
     </BrowserRouter>
   )
