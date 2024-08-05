426f1b8ec5b61545660af4e7b56336d7bb33f321
GAURAV <gaurav1993rajput@gmail.com>
2024-08-05 19:55:52 +0530

    Inital set-up for client

---
+++ chatAppClient/src/App.jsx
@@ -0,0 +1,18 @@
+import React, {lazy} from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Home = lazy(()=>import("./pages/Home"))
+const Login = lazy(()=>import("./pages/Login"))
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App 
